57d188531f69792189bd5e7ca97d1879e891aceb
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-13 01:08:11 +0530

    updating

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ var fs = require('fs');
 var build = require('build');
 var http = require('http');
 var express = require('express');
-var proxy = require('proxy');
 var agent = require('hub-agent');
 
 var PORT = 4002;
 var app = express();
-
-var allow = {
-    'accounts.serandives.com': 4004,
-    'auto.serandives.com': 4004,
-    'localhost': 4004
-};
-proxy = proxy(allow);
 
 var index = fs.readFileSync('./public/index.html', 'utf-8');
 
@@ -21,7 +13,7 @@ app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use('/public', express.static(__dirname + '/public'));
 
 //proxying requests
-app.use(proxy);
+app.use(agent.proxy());
 
 //hot building component
 app.use(build);
