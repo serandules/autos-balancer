cf63876fdcab822d9289006c30fb2f8563d33e4e
ruchiraw <ruchira.wageesha@gmail.com>
2015-01-10 20:25:44 +0530

    updating

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 var debug = require('debug')('serandules:autos');
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
 var agent = require('hub-agent');
 
-var HTTP_PORT = 4004;
+agent(function () {
+    var fs = require('fs');
+    var http = require('http');
+    var express = require('express');
 
-var dev = process.env.NODE_ENV === 'development';
+    var HTTP_PORT = 4004;
 
-var app = express();
+    var app = express();
 
-var index = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
+    var index = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
 
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use('/public', express.static(__dirname + '/public'));
+    app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+    app.use('/public', express.static(__dirname + '/public'));
 
-//proxying requests
-app.use(agent.proxy());
+    //proxying requests
+    app.use(agent.proxy());
 
-//hot building component
-app.use(require('build'));
+    //hot building component
+    app.use(require('build'));
 
-//index page
-app.all('*', function (req, res) {
-    //TODO: check caching headers
-    res.set('Content-Type', 'text/html').send(200, index);
+    //index page
+    app.all('*', function (req, res) {
+        //TODO: check caching headers
+        res.set('Content-Type', 'text/html').send(200, index);
+    });
+
+    //error handling
+    app.use(agent.error);
+
+    http.createServer(app).listen(HTTP_PORT);
 });
-
-//error handling
-app.use(agent.error);
-
-agent(http.createServer(app), HTTP_PORT);
 
 process.on('uncaughtException', function (err) {
     debug('unhandled exception ' + err);
@@ -38,8 +39,8 @@ process.on('uncaughtException', function (err) {
 });
 
 /*
-setTimeout(function () {
-    agent.config('ruchira', function (data) {
-        debug(data);
-    });
-}, 0);*/
+ setTimeout(function () {
+ agent.config('ruchira', function (data) {
+ debug(data);
+ });
+ }, 0);*/
