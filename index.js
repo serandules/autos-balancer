713de4d144fe32920b1aa6c01eca57575983fdf5
serandules <serandules@gmail.com>
2015-11-20 11:11:53 +0530

    adding auth fixies

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ clustor(function () {
     var http = require('http');
     var express = require('express');
     var favicon = require('serve-favicon');
+    var bodyParser = require('body-parser');
     var builder = require('component-middleware');
     var agent = require('hub-agent');
     var proxy = require('proxy');
     var procevent = require('procevent')(process);
     var utils = require('utils');
+    var dust = require('dustjs-linkedin');
     var build = require('build');
 
     var prod = utils.prod();
 
     var index = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
+
+    dust.loadSource(dust.compile(index, 'index'));
 
     var app = express();
 
@@ -37,6 +41,30 @@ clustor(function () {
             path: '/public/build'
         }));
     }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    //index page with embedded oauth tokens
+    app.post('/auth/oauth', function (req, res) {
+        var context = {
+            username: req.body.username,
+            access: req.body.access_token,
+            expires: req.body.expires_in,
+            refresh: req.body.refresh_token
+        };
+        //TODO: check caching headers
+        dust.render('index', context, function (err, index) {
+            if (err) {
+                log.error(err);
+                res.status(500).send({
+                    error: 'error rendering requested page'
+                });
+                return;
+            }
+            res.set('Content-Type', 'text/html').status(200).send(index);
+        });
+    });
 
     //index page
     app.all('*', function (req, res) {
